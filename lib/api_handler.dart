70e3bb1f1cf107ff7442c26d4a85a70713b4919c
Mariano Carrazana <marianocarrazana@gmail.com>
2023-07-30 16:05:39 -0300

    Refactor api_handler

--- lib/api_handler.dart
+++ lib/api_handler.dart
@@ -1,41 +1,28 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'dart:typed_data';
 
 import 'package:dartssh2/dartssh2.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SSH {
-  const SSH({this.client});
-  final SSHClient? client;
-}
+SSHClient? sshclient;
 
-class SSHProxy extends StateNotifier<SSH> {
-  SSHProxy() : super(const SSH());
-  void setClient(SSHClient newClient) async {
-    state = SSH(client: newClient);
-  }
-}
-
-final sshProvider = StateNotifierProvider<SSHProxy, SSH>((ref) {
-  return SSHProxy();
-});
-
-Future<dynamic> apiGet(Ref ref, String method, String url,
+Future<dynamic> apiGet(Ref ref, String method, String path,
     {Map<String, dynamic>? queryParameters}) async {
   final prefs = await SharedPreferences.getInstance();
-  if (ref.read(sshProvider).client == null) {
-    final newClient = SSHClient(
+  var client = sshclient;
+  if (client == null) {
+    log("Creating connection...");
+    client = SSHClient(
       await SSHSocket.connect(prefs.getString("Host") ?? "", 22),
       username: prefs.getString("Username") ?? "",
       onPasswordRequest: () => prefs.getString("Password"),
     );
-    ref.read(sshProvider.notifier).setClient(newClient);
+    sshclient = client;
   }
-  SSHClient? client = ref.read(sshProvider).client;
-  if (client == null) return null;
-  final uri = Uri.http('localhost', '/v1.25/$url/json', queryParameters);
-  final output = await client
+  final Uri uri = Uri.http('localhost', '/v1.25/$path/json', queryParameters);
+  final Uint8List output = await client
       .run('curl --unix-socket /var/run/docker.sock "$uri"', stderr: false);
   String decoded = utf8.decode(output);
   dynamic obj = jsonDecode(decoded);
